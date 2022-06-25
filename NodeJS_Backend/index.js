01d41c1839971128bfd18c8eaf998dcc20164f94
Ahmad Othman <aothman156@gmail.com>
2022-06-25 15:47:41 +0300

    add:connection to mongodb

---
+++ NodeJS_Backend/index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const userRouter = require('./src/user');
+const contactRouter = require('./src/contact');
+
+const DB_CONNECT = process.env.DB_CONNECT ;
+mongoose.connect(DB_CONNECT,{
+    useNewUrlParser:true,useUnifiedTopology:true
+},(err)=>{
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log("successfully connected")
+    }
+})
+
+
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/user', userRouter);
+app.use('/api/contact', contactRouter);
+
+app.listen(3000, () => {
+    console.log('server running on port 3000')
+})
